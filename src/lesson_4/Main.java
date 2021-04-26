a56b6c233d7e7ba925cc6fd4f862d1020759d9f9
Sasha <zvezdopad659527@gmail.com>
2021-04-26 21:12:31 +0500

    поочередный вывод букв A, B и C (ABCABCABC)

--- src/lesson_4/Main.java
+++ src/lesson_4/Main.java
@@ -1,22 +1,19 @@
 package lesson_4;
 
-//Создать три потока, каждый из которых выводит определенную букву (A, B и C)
-//5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.
-
 public class Main {
     public static void main(String[] args) {
         Printer printer = new Printer();
+        int count = 5;
 
-        final Thread a = new Thread(() -> {
-            printer.print('A', 5);
-        });
+        final Thread a = new Thread(() -> printer.print('A', count));
 
-        final Thread b = new Thread(() -> {
-            printer.print('B', 5);
-        });
+        final Thread b = new Thread(() -> printer.print('B', count));
+
+        final Thread c = new Thread(() -> printer.print('C', count));
 
         a.start();
         b.start();
+        c.start();
 
     }
 }
--- src/lesson_4/Printer.java
+++ src/lesson_4/Printer.java
@@ -3,17 +3,17 @@ package lesson_4;
 public class Printer {
     private boolean isA = true;
     private boolean isB = false;
+    private boolean isC = false;
 
     public void print(char ch, int count) {
         synchronized (this) {
-
             for (int i = 0; i < count; i++) {
                 while (isA) {
                     if (ch == 'A') {
                         System.out.print(ch);
                         isA = false;
                         isB = true;
-                        notify();
+                        notifyAll();
                     }
                     if (ch != 'A') {
                         try {
@@ -26,9 +26,9 @@ public class Printer {
                 while (isB) {
                     if (ch == 'B') {
                         System.out.print(ch);
-                        isA = true;
                         isB = false;
-                        notify();
+                        isC = true;
+                        notifyAll();
                     }
                     if (ch != 'B') {
                         try {
@@ -38,8 +38,22 @@ public class Printer {
                         }
                     }
                 }
+                while (isC) {
+                    if (ch == 'C') {
+                        System.out.print(ch);
+                        isC = false;
+                        isA = true;
+                        notifyAll();
+                    }
+                    if (ch != 'C') {
+                        try {
+                            wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
             }
-
         }
 
     }
