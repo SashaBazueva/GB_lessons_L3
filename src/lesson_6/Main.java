165fb695529c60f639b5cba89cc497dd211ce2ef
Sasha <zvezdopad659527@gmail.com>
2021-05-08 19:26:53 +0500

    написала тесты, написала проверяемые методы

--- src/lesson_6/Main.java
+++ src/lesson_6/Main.java
@@ -1,48 +1,64 @@
 package lesson_6;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    private Main(){
+    private Main() {
 
-    }
-
-    public static void main(String[] args) {
-        int[] arr1 = {5,3,4,2,7,6,4,4,3,1};
-        int[] result1 = {3,1};
-        int[] array1 = returnIntArrayUnderLast4(arr1);
-        for (int numb: array1) {
-            System.out.print(numb);
-        }
     }
 
     /*
     Метод возвращает массив, полученный путем удаления чисел перед последней четверкой включительно.
      */
 
-    public static int[] returnIntArrayUnderLast4 (int[] arrayOfNumbers){
+    public static int[] returnIntArrayAfterLast4(int[] arrayOfNumbers) {
         List<Integer> copy = new ArrayList<>();
 
         byte count = 0;
-        for (int numb: arrayOfNumbers) {
-            if (numb==4){
+        for (int numb : arrayOfNumbers) {
+            if (numb == 4) {
                 count++;
             }
             copy.add(numb);
         }
-
-        for (int i = 0; count!=1; i++) {
-            if(copy.get(i)==4){
-                count--;
-            }
-            copy.remove(i);
+        if (count == 0) {
+            throw new RuntimeException("Array doesn't contain number 4");
         }
 
+        int ind = 0;
+        do {
+            if (copy.get(ind) == 4) {
+                count--;
+            }
+            copy.remove(ind);
+        } while (count > 0);
+
         int[] result = new int[copy.size()];
-        for (int i = 0; i < copy.size()-1; i++) {
+        for (int i = 0; i < copy.size(); i++) {
             result[i] = copy.get(i);
         }
 
         return result;
+    }
+
+    /*
+    Метод возвращает:
+    true - если в массиве есть минимум одна еденица и одна четверка,
+    false - если в массиве нет хоть одной четверки или единицы.
+     */
+    public static boolean checkIntArrayOnPresence1And4(int[] arrayOfNumbers) {
+        boolean hasOne = false;
+        boolean hasFour = false;
+        for (int numb : arrayOfNumbers) {
+            if (numb == 1) {
+                hasOne = true;
+            }
+            if (numb == 4) {
+                hasFour = true;
+            }
+        }
+
+        return hasOne && hasFour;
     }
 }
--- src/lesson_6/MainTest.java
+++ src/lesson_6/MainTest.java
@@ -6,13 +6,42 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
 
     @Test
-    public void shouldDeleteAllNumbersBeforeFourAndFour() {
-        int[] arr1 = {5,3,4,2,7,6,4,4,3,1};
-        int[] result1 = {3,1};
-        Assertions.assertArrayEquals(result1, Main.returnIntArrayUnderLast4(arr1));
+    public void shouldDeleteAllNumbersInArrayExceptNumbersAfterLastFour() {
+        int[] arr1 = {5, 3, 4, 2, 7, 6, 4, 4, 3, 1};
+        int[] result1 = {3, 1};
+        Assertions.assertArrayEquals(result1, Main.returnIntArrayAfterLast4(arr1));
 
-        int[] arr2 = {1,4,3,5,2,6,4,4,0,2,3};
-        int[] result2 = {0,2,3};
-        Assertions.assertArrayEquals(result2, Main.returnIntArrayUnderLast4(arr2));
+        int[] arr2 = {1, 4, 3, 5, 2, 6, 4, 4, 0, 2, 3};
+        int[] result2 = {0, 2, 3};
+        Assertions.assertArrayEquals(result2, Main.returnIntArrayAfterLast4(arr2));
+
+        int[] arr3 = {4, 4, 2, 5, 4, 3, 6, 4, 0, 8, 1};
+        int[] result3 = {0, 8, 1};
+        Assertions.assertArrayEquals(result3, Main.returnIntArrayAfterLast4(arr3));
+
+        int[] arr4 = {4, 4, 4, 4, 4, 4};
+        int[] result4 = new int[0];
+        Assertions.assertArrayEquals(result4, Main.returnIntArrayAfterLast4(arr4));
+    }
+
+    @Test
+    public void shouldThrowRunTimeExceptionIfArrayDoesNotContainNumberFour() {
+        int[] arr = {1, 2, 3, 5, 6, 7, 8, 9, 0};
+        Assertions.assertThrows(RuntimeException.class, () -> Main.returnIntArrayAfterLast4(arr));
+    }
+
+    @Test
+    public void shouldCheckIfArrayHasNumbersOneAndFour() {
+        int[] arr1 = {5, 3, 4, 2, 7, 6, 4, 4, 3, 1};
+        Assertions.assertTrue(Main.checkIntArrayOnPresence1And4(arr1));
+
+        int[] arr2 = {1, 4, 3, 5, 2, 6, 4, 4, 0, 2, 3};
+        Assertions.assertTrue(Main.checkIntArrayOnPresence1And4(arr2));
+
+        int[] arr3 = {4, 4, 2, 5, 4, 3, 6, 4, 0, 8};
+        Assertions.assertFalse(Main.checkIntArrayOnPresence1And4(arr3));
+
+        int[] arr4 = {4, 4, 4, 4, 4, 4};
+        Assertions.assertFalse(Main.checkIntArrayOnPresence1And4(arr4));
     }
 }
