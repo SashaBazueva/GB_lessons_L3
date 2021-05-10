0cb2c88cfc86b5441f197ee2f25b8ae10bad52b0
Sasha <zvezdopad659527@gmail.com>
2021-05-10 21:27:53 +0500

    Создала необходимые анотации, testRunner и класс с тестируемыми методами

---
+++ src/lesson_7/Main.java
@@ -0,0 +1,14 @@
+package lesson_7;
+
+public class Main {
+
+    @Test
+    public boolean containSequence(int[] arrayOfNumbers){
+        return false;
+    }
+
+    @Test
+    public boolean containOne(int[] arrayOfNumbers){
+        return false;
+    }
+}
---
+++ src/lesson_7/TestRunner.java
@@ -0,0 +1,11 @@
+package lesson_7;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        start(Main.class);
+    }
+
+    public static void start(Class testClass){
+
+    }
+}
