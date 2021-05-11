04605410faf7ae3209d6bd89972e05c103ffc052
Sasha <zvezdopad659527@gmail.com>
2021-05-11 23:13:05 +0500

    выполняет только один тип аннотации(или @Test, или @AfterSuite, или @BeforeSuite)

--- src/lesson_7/Main.java
+++ src/lesson_7/Main.java
@@ -1,14 +1,46 @@
 package lesson_7;
 
 public class Main {
+    public static void main(String[] args) {
+        TestRunner.start(Main.class);
+    }
 
-    @Test
-    public boolean containSequence(int[] arrayOfNumbers){
-        return false;
+    @BeforeSuite
+    public void printArray(int[] arrayOfNumbers) {
+        System.out.print("{ ");
+        for (int i = 0; i < arrayOfNumbers.length - 1; i++) {
+            System.out.print(arrayOfNumbers[i] + ", ");
+        }
+        System.out.print(arrayOfNumbers[arrayOfNumbers.length - 1] + " } ");
     }
 
     @Test
-    public boolean containOne(int[] arrayOfNumbers){
-        return false;
+    public void countQuantity(int[] arrayOfNumbers) {
+        System.out.println("Длина тестируемого массива: " + arrayOfNumbers.length);
     }
+
+    @Test
+    public void checkIfContainTwo(int[] arrayOfNumbers) {
+        boolean hasTwo = false;
+        for (int number : arrayOfNumbers) {
+            if (number == 2) {
+                hasTwo = true;
+                System.out.println("Содержит двойку: " + hasTwo);
+                break;
+            }
+        }
+        if (!hasTwo) {
+            System.out.println("Содержит двойку: " + hasTwo);
+        }
+    }
+
+    @AfterSuite
+    public void calculateSum(int[] arrayOfNumbers) {
+        int sum = 0;
+        for (int number : arrayOfNumbers) {
+            sum += number;
+        }
+        System.out.println("Сумма чисел массива: " + sum);
+    }
+
 }
--- src/lesson_7/TestRunner.java
+++ src/lesson_7/TestRunner.java
@@ -1,11 +1,47 @@
 package lesson_7;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class TestRunner {
-    public static void main(String[] args) {
-        start(Main.class);
+
+    static int[] testArray = {1, 2, 3, 4, 6, 1, 4, 8, 9, 5};
+
+    public static void start(Class testClass) {
+
+        new Thread(new Clazz(testClass)).start();
+//        new Thread(new Clazz(testClass)).start();
+//        new Thread(new Clazz(testClass)).start();
+    }
+}
+class Clazz implements Runnable{
+
+    Class testClass;
+
+    public Clazz(Class testClass) {
+        this.testClass = testClass;
     }
 
-    public static void start(Class testClass){
+    @Override
+    public void run() {
+        final Method[] methods = testClass.getDeclaredMethods();
 
+        for (Method method : methods) {
+
+            Annotation[] annotations = method.getAnnotations();
+            for (Annotation annotation : annotations) {
+
+                if (annotation.annotationType().equals(Test.class)) {
+                    try {
+                        method.invoke(testClass.getDeclaredConstructor().newInstance(), TestRunner.testArray);
+                    } catch (IllegalAccessException | InvocationTargetException | InstantiationException | NoSuchMethodException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+        }
     }
 }
